972f028c383fc3b46dae803a82a799b380c54ca4
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-12-28 14:25:01 +0530

    Update page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -156,7 +156,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-16 border-2 ">
+      <div className="flex flex-col gap-16  ">
         <div className="flex flex-col gap-5">
           <h3 className="text-2xl text-secondary font-semibold">
             Product Description
